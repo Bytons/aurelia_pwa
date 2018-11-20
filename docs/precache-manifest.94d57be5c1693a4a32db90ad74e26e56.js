9f54c99394cd5aa550d34d76be5ae7e2dc4c752b
Antti <antti.hakkinen94@gmail.com>
2018-11-20 14:31:22 +0200

    setup showcase viewing

---
+++ docs/precache-manifest.94d57be5c1693a4a32db90ad74e26e56.js
@@ -0,0 +1,16 @@
+self.__precacheManifest = [
+  {
+    "url": "/vendor.30806019b9cef3def41c.bundle.js"
+  },
+  {
+    "revision": "33a694cc81c8645ab12375d905517d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c3a1b8ada90aaf6e31a669a57e3085a",
+    "url": "/favicon.ico"
+  },
+  {
+    "url": "/app.d08b85a73eabe36d7e7e.bundle.js"
+  }
+];
