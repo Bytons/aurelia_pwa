737174df48f718af2f5685b2d046ca1732ef62c5
Antti <antti.hakkinen94@gmail.com>
2018-11-27 10:52:06 +0200

    fix build error

--- docs/precache-manifest.3362ad095d33f59294e32a7a3f80c8e3.js
+++ docs/precache-manifest.3362ad095d33f59294e32a7a3f80c8e3.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "94b8bf7d11e952bd92520e385859c804",
-    "url": "/aurelia_pwa/manifest/images/icons/icon-192x192.png"
+    "revision": "37edbda521e97639f9f87453b06c0fc2",
+    "url": "/aurelia_pwa/manifest/images/icons/icon-152x152.png"
   },
   {
     "revision": "7aa4f3951a792e898a4277c5c07ff922",
@@ -23,8 +23,8 @@ self.__precacheManifest = [
     "url": "/aurelia_pwa/manifest/images/icons/icon-384x384.png"
   },
   {
-    "revision": "37edbda521e97639f9f87453b06c0fc2",
-    "url": "/aurelia_pwa/manifest/images/icons/icon-152x152.png"
+    "revision": "94b8bf7d11e952bd92520e385859c804",
+    "url": "/aurelia_pwa/manifest/images/icons/icon-192x192.png"
   },
   {
     "revision": "596beb3e51048d20b31914d5a480e0dc",
@@ -35,7 +35,7 @@ self.__precacheManifest = [
     "url": "/aurelia_pwa/manifest/images/icons/icon-128x128.png"
   },
   {
-    "revision": "51fcebd48b04f49968d405db10d175ee",
+    "revision": "113b511c7dc8b024ec1010e82e31556a",
     "url": "/aurelia_pwa/index.html"
   },
   {
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/aurelia_pwa/precache-manifest.5e0beeb5966f87531f8c4d54609210a7.js", "/aurelia_pwa/workbox-v3.6.3/workbox-sw.js");
+importScripts("/aurelia_pwa/precache-manifest.3362ad095d33f59294e32a7a3f80c8e3.js", "/aurelia_pwa/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/aurelia_pwa/workbox-v3.6.3"});
 // disable/enable debug logging
 workbox.setConfig({ debug: true });
